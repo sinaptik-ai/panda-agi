694cd159fa4d8f4db02dbaf46a1ae7b88478ed3d
Gabriele Venturi <lele.venturi@gmail.com>
2025-06-19 23:42:33 +0200

    chore: rename custom to lite

--- panda_agi/client/agent.py
+++ panda_agi/client/agent.py
@@ -44,7 +44,7 @@ class Agent:
         self,
         host: str = "wss://agi-api.pandas-ai.com",
         api_key: str = None,
-        llm_type: Literal["custom", "pro"] = "pro",
+        llm_type: Literal["lite", "pro"] = "pro",
         conversation_id: Optional[str] = None,
         auto_reconnect: bool = False,
         reconnect_interval: float = 5.0,
