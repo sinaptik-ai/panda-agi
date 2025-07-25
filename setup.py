ffe127c57e679a11dd3a7da8e233652077f9e9a6
Gabriele Venturi <lele.venturi@gmail.com>
2025-07-25 10:13:03 +0200

    Release v0.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="panda_agi",
-    version="0.6.1",
+    version="0.6.2",
     description="PandaAGI SDK - API for AGI",
     author="PandaAGI Team",
     packages=find_packages(),
