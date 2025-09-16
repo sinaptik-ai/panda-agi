5dd30e97703fa7cd0401c09927223f99ad165127
ArslanSaleem <khan.arslan38@gmail.com>
2025-09-16 18:12:58 +0200

    fix: csv multiple files

--- examples/ui/backend/utils/exceptions.py
+++ examples/ui/backend/utils/exceptions.py
@@ -4,3 +4,15 @@ class FileNotFoundError(Exception):
 
 class RestrictedAccessError(Exception):
     pass
+
+
+class PXMLParsingError(Exception):
+    """Raised when there's an error parsing PXML content"""
+
+    pass
+
+
+class CSVFileError(Exception):
+    """Raised when there's an error accessing or processing CSV files"""
+
+    pass
